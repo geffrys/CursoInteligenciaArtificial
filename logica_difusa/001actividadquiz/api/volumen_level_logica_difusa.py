5eda9345799f0ca2da206cbe1c41c14c4a6cf4fb
Geffry Alejandro Ospina Atehortúa <geffry.ospina@gmail.com>
2024-09-01 22:18:50 -0500

    rules nuevas para estado normal, y correccion de consequent y antecedents

--- logica_difusa/001actividadquiz/api/volumen_level_logica_difusa.py
+++ logica_difusa/001actividadquiz/api/volumen_level_logica_difusa.py
@@ -5,9 +5,9 @@ import matplotlib.pyplot as plt
 
 def evaluate_(hora: any, estado_animo: any): 
     # Definir los antecedentes (input variables) Fuzzificacion.
-    volume = ctrl.Antecedent(np.arange(0, 11, 1), 'volume')
     hour = ctrl.Antecedent(np.arange(0, 25, 1), 'hour')
     mood = ctrl.Antecedent(np.arange(0, 11, 1), 'mood')  # 0 a 10 (0: triste, 10: feliz)
+    volume = ctrl.Consequent(np.arange(0, 11, 1), 'volume')
     # Fin de fuzzificacion.
 
     # Definir las funciones de membresía para cada antecedente
@@ -15,14 +15,14 @@ def evaluate_(hora: any, estado_animo: any):
     volume['medio'] = fuzz.trimf(volume.universe, [3, 5, 7])
     volume['alto'] = fuzz.trimf(volume.universe, [6, 8, 10])
 
-    hour['madrugada'] = fuzz.trimf(hour.universe, [0, 3, 6])
-    hour['manana'] = fuzz.trimf(hour.universe, [5, 10 , 13])
-    hour['tarde'] = fuzz.trimf(hour.universe, [12, 15, 18])
-    hour['noche'] = fuzz.trimf(hour.universe, [17, 21, 24]
+    hour['madrugada'] = fuzz.trapmf(hour.universe, [0, 2, 4, 6])
+    hour['manana'] = fuzz.trapmf(hour.universe, [5, 7 , 11, 13])
+    hour['tarde'] = fuzz.trapmf(hour.universe, [12, 14, 16, 18])
+    hour['noche'] = fuzz.trapmf(hour.universe, [17, 19, 22, 24]
                                )
-    mood['triste'] = fuzz.trimf(mood.universe, [0, 2, 4])
-    mood['normal'] = fuzz.trimf(mood.universe, [3, 5, 7])
-    mood['feliz'] = fuzz.trimf(mood.universe, [6,8 , 10])
+    mood['triste'] = fuzz.trapmf(mood.universe, [0, .5, 3.5, 4])
+    mood['normal'] = fuzz.trapmf(mood.universe, [3, 3.5, 6.5, 7])
+    mood['feliz'] = fuzz.trapmf(mood.universe, [6, 6.5, 9.5, 10])
     # Fin de definicion de memberships
 
     # Visualización de las funciones de membresía
@@ -43,7 +43,12 @@ def evaluate_(hora: any, estado_animo: any):
     rule6 = ctrl.Rule(hour['tarde'] & mood['feliz'], volume['alto'])
     rule7 = ctrl.Rule(hour['noche'] & mood['triste'], volume['bajo'])
     rule8 = ctrl.Rule(hour['noche'] & mood['feliz'], volume['alto'])
-    volume_ctrl = ctrl.ControlSystem([rule1, rule2, rule3, rule4, rule5, rule6, rule7, rule8])
+    rule9 = ctrl.Rule(hour['madrugada'] & mood['normal'], volume['medio'])
+    rule10 = ctrl.Rule(hour['manana'] & mood['normal'], volume['medio'])
+    rule11 = ctrl.Rule(hour['tarde'] & mood['normal'], volume['alto'])
+    rule12 = ctrl.Rule(hour['noche'] & mood['normal'], volume['alto'])
+
+    volume_ctrl = ctrl.ControlSystem([rule1, rule2, rule3, rule4, rule5, rule6, rule7, rule8, rule9, rule10, rule11, rule12])
     # Fin reglas difusas
 
     # Crear la simulación del sistema de control
@@ -52,11 +57,13 @@ def evaluate_(hora: any, estado_animo: any):
     # Evaluar el volumen basado en hora y estado de ánimo
     volume_level.input['hour'] = hora
     volume_level.input['mood'] = estado_animo
-    volume_level.input['volume'] = None
-    print(f"Inputs asignados: Hora - {hora}, Estado de ánimo - {estado_animo}")
     volume_level.compute()
 
+    result = volume_level.output['volume']
+    print(f'El volumen del sistema de audio es: {result}')
+
     volume.view(sim=volume_level)
+    plt.title('Volumen del sistema de audio')
     plt.show()
 
-evaluate_(5, 4)
+evaluate_(2, 3)
